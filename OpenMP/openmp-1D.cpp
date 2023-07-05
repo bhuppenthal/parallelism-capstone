d5a19ad668027708d27e1b7b8831b0374ded76ad
getekj <joannagetek@gmail.com>
2023-07-05 10:03:53 -0700

    Added variable/function declarations

--- OpenMP/openmp-1D.cpp
+++ OpenMP/openmp-1D.cpp
@@ -11,6 +11,32 @@
 #include <math.h>
 #include <omp.h>
 
+#define NUM_TIME_STEPS  100                     // number of time steps the simultation runs through
+
+#ifndef NUME
+#define NUME            1024                    // total number of elements
+#endif 
+
+#ifndef NUMT
+#define NUMT            4                       // number of threads to use
+#endif
+
+#define NUM_ELEM_PER_THREAD    (NUME/NUMT)      // number of elements in each thread
+
+float   Temps[2][NUME];                         // storing all temperatures "Now" and "Next" states
+
+int     Now;                                    // which array is the "current values" = 0 or 1
+int     Next;                                   // which array is being filled = 1 or 0
+
+void    DoAllWork(int);
+
 int main(void) {
+
     printf("Hello world\n");
+    
+}
+
+void DoAllWork(int me) {
+    
+
 }
