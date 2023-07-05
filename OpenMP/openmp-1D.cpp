10dccd25eb727d1a1152e3d0fc788deb7c2f59c9
getekj <joannagetek@gmail.com>
2023-07-05 09:04:55 -0700

    Citation added

--- OpenMP/openmp-1D.cpp
+++ OpenMP/openmp-1D.cpp
@@ -1,3 +1,12 @@
+/*
+
+    Heat Diffusion Equation Simulation - 1D Using OpenMP
+    Code Obtained From:
+    Bailey, Mike. (2023). Data Decomposition Slides 6 - 9. Retrieved on 07/05/2023 From:
+    https://web.engr.oregonstate.edu/~mjb/cs575/Handouts/data_decomposition.1pp.pdf 
+
+*/
+
 #include <stdio.h>
 #include <math.h>
 #include <omp.h>
