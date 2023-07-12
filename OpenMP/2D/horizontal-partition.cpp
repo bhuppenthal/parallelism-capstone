ce9d523d83323366cc9b1632e9a556a120e2e01f
huppentb <huppentb@oregonstate.edu>
2023-07-11 17:11:43 -0700

    Create partition.h
    Create a header file to hold constants and equations used by all implementations.

--- OpenMP/2D/horizontal-partition.cpp
+++ OpenMP/2D/horizontal-partition.cpp
@@ -9,21 +9,7 @@
 #include <omp.h>
 #include <stdbool.h>
 
-#define NUM_TIME_STEPS  100                      // number of time steps the simultation runs through
-
-#ifndef NUME
-#define NUME            64                    // total number of elements
-#endif
-
-#ifndef NUMT
-#define NUMT            4                       // number of threads to use
-#endif
-
-#define NUM_ELEM_PER_THREAD    (NUME/NUMT)      // number of elements in each thread
-
-#ifndef PRINT_ALL_TIME_STEPS
-#define PRINT_ALL_TIME_STEPS		true         // set to true to allow all time steps to print
-#endif                            
+#include "partition.h"                           
 
 // TODO: add a CACHE_ALIGNMENT constant
 // 0 to 63, experiment with various misalignments to see results
@@ -36,19 +22,6 @@ float   Temps[2][SIDE][SIDE];
 int     Now;                                    // which array is the "current values" = 0 or 1
 int     Next;                                   // which array is being filled = 1 or 0
 
-// Heat Diffusion Equation Constants
-const float RHO = 8050;
-const float C = 0.466;
-const float K = 20;
-float k_over_rho_c = K/(RHO*C);                 // units of m^2/s NOTE: Cannot be a const (true for OpenMP too?)
-// K/(RHO*C) = 5.33 x 10^-6 m^2/s
-
-const float DX = 1;
-const float DY = 1;
-const float DT = 1;
-
-// Heat Diffusion Equation
-#define CALC_DTEMP(elem, left, right, up, down) (k_over_rho_c * DT * ((down - 2*elem + up)/(DY * DY) + (left - 2*elem + right)/(DX * DX)));
 
 void    DoAllWork(int);
 
