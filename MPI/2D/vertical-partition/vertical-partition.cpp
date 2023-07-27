988e7e6bc02ab3fdef028805269861c864e227b6
getekj <joannagetek@gmail.com>
2023-07-27 13:59:49 -0700

    Added vertical partition folder and file

---
+++ MPI/2D/vertical-partition/vertical-partition.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#include <mpi.h>
+#include <string.h>
+#include <iostream>
+#include <iomanip>
+#include "heat.h"
+
+
+int main(int argc, char *argv[]) {
+    printf("hello world");
+}
